65518983660d06ba785c84e8a658d787f17d1a93
unknown <akash.mulik605@gmail.com>
2025-01-24 22:56:13 +0530

    change

--- src/pages/template/footer.jsx
+++ src/pages/template/footer.jsx
@@ -1,7 +1,48 @@
 import React from 'react'
 
-export const footer = () => {
+ const Footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+        <div className='footer'>
+            <div className='sb_section_padding'>
+                <div className='sb_footer-links'>
+                    <div className='sb_footer-links-div'>
+                        <h4>AKash</h4>
+                        <a href='/'>
+                            <p>Employee</p>
+                        </a>
+                        <a href='/'>
+                            <p>Employee</p>
+                        </a>
+                        <a href='/'>
+                            <p>xyz</p>
+                        </a>
+                        <a href='/'>
+                            <p>abc</p>
+                        </a>
+                    </div>
+                    <div className='sb_fotter-links-div'>
+                        <h4>AKash</h4>
+                        <a href='/'>
+                            <p>Employee</p>
+                        </a>
+                        <a href='/'>
+                            <p>Employee</p>
+                        </a>
+                        <a href='/'>
+                            <p>xyz</p>
+                        </a>
+                        <a href='/'>
+                            <p>abc</p>
+                        </a>
+                    </div>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </footer>
   )
 }
+
+export default Footer
